fbf8624ed6e7daa76b4ce00b6b78f3d1ef51f143
Trevor <trevorcolexander@gmail.com>
2018-02-17 02:50:38 -0500

    Initial Commit

---
+++ StackC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  StackC
+//
+//  Created by Trevor Xander on 2/17/18.
+//  Copyright © 2018 Trevor Xander. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
